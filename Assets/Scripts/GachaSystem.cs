3d951787362c91efbb046e72e73f0d344f52f6aa
NaritaShosei <onigirikun7171@gmail.com>
2025-10-25 17:19:46 +0900

    [udpate]APIからTextを取得

---
+++ Assets/Scripts/GachaSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GachaSystem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _text;
+
+    private string _apiKey = "http://localhost:8080/health";
+
+    private void Start()
+    {
+        StartCoroutine(Get());
+    }
+
+    private IEnumerator Get()
+    {
+        UnityWebRequest www = UnityWebRequest.Get(_apiKey);
+
+        yield return www.SendWebRequest();
+
+        if (www.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError("Advice API error: " + www.error);
+            yield break;
+        }
+
+        string json = www.downloadHandler.text;
+        Debug.Log(json);
+        try
+        {
+            _text.text = json;
+        }
+        catch { _text.text = "おなかすいたかもね"; }
+    }
+}
